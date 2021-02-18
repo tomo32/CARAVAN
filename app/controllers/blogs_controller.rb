70b44f11161e71803311d9f1236a2e0d9d78739c
tomo32 <19da018k@rikkyo.ac.jp>
2021-02-18 16:05:07 +0000

    [Finish] 4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -20,7 +20,13 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
